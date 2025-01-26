ff8d67a9dd585d405c883cc0a1d4850e8725d930
unknown <tommasoandres123@gmail.com>
2025-01-26 11:54:53 +0100

    fixini

--- Assets/Game/Scripts/UI/MyButton.cs
+++ Assets/Game/Scripts/UI/MyButton.cs
@@ -6,16 +6,26 @@ using UnityEngine.UI;
 
 public class MyButton : MonoBehaviour
 {
+    [SerializeField] private Button[] buttons;
     [SerializeField] private Image image;
     [SerializeField] private Color onHoverColor;
 
     public void ChangeColor()
     {
+        AllButtonsDefaultColor();
         image.color = onHoverColor;
     }
 
     public void BackToDefaultColor()
     {
         image.color = Color.white;
+    }
+
+    private void AllButtonsDefaultColor()
+    {
+        foreach (var button in buttons)
+        {
+            button.image.color = Color.white;
+        }
     }
 }
