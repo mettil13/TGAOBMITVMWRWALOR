f7b9e94e828a0479740a95c962ad88ac2c466ded
mettil13 <matildemariano01@gmail.com>
2025-01-26 13:12:56 +0100

    UI mannequin damage

--- Assets/Game/Scripts/UI/UIMannequin.cs
+++ Assets/Game/Scripts/UI/UIMannequin.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,12 +6,34 @@ using UnityEngine.UI;
 
 public class UIMannequin : MonoBehaviour
 {
+    public Color damageColor;
+    public Color normalColor;
     public List<Image> bodyParts;
+    private Tweener[] bodyPartsTween;
+    public float fadeDuration = 2;
 
 
     void Start()
     {
+        proceduralGeneration.RiverGeneration.instance.playerDatas.damageTaken.AddListener(RegisterDamage);
+        bodyPartsTween = new Tweener[bodyParts.Count];
         
+    }
+
+    public void RegisterDamage(string name, float amount) { // max amount is 5
+        for(int i = 0; i < bodyParts.Count; i++) {
+            if(bodyParts[i].name.ToLower() == name.ToLower()) {
+                if(bodyPartsTween[i] != null) {
+                    bodyPartsTween[i].Kill();
+                }
+                //Color newColor = normalColor * (1 - amount / 5) + damageColor * (amount / 5);
+                Color newColor = damageColor;
+                bodyParts[i].color = newColor;
+                bodyPartsTween[i] = bodyParts[i].DOColor(normalColor, fadeDuration);
+                
+            }
+        }
+
     }
 
 }
